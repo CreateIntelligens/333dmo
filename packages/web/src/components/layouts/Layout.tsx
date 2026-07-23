aa265068d8e945687f8c7d5e75227b5b6b357c38
tbdavid2019 <tbdavid2019@gmail.com>
2026-07-23 17:25:32 +0800

    style: refine dashboard shell baseline

--- packages/web/src/components/layouts/Layout.tsx
+++ packages/web/src/components/layouts/Layout.tsx
@@ -4,11 +4,11 @@ import MarqueeHeader from '../real-time/MarqueeHeader';
 
 export default function Layout() {
   return (
-    <div className="flex flex-col h-screen overflow-hidden">
+    <div className="flex min-h-dvh flex-col overflow-hidden bg-primary">
       <MarqueeHeader />
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex min-h-0 flex-1 overflow-hidden">
         <Sidebar />
-        <main className="flex-1 overflow-y-auto p-5 lg:p-6">
+        <main className="min-w-0 flex-1 overflow-y-auto p-4 sm:p-5 lg:p-6">
           <Outlet />
         </main>
       </div>
